db189895022c4a8c9d32ebec42b7eac7cd426193
igarashi <igarashi@kuis.kyoto-u.ac.jp>
2016-11-13 18:48:34 +0900

    Add 'public' modifier to interface.

--- java/Tree.java
+++ java/Tree.java
@@ -1,4 +1,4 @@
-interface Tree {
+public interface Tree {
     public int value();
     // public String toString();             // Kadai 1-3
     // public String toStringFewerParens();  // Kadai 1-4
