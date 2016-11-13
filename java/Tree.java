a843c026181b529d2a5346d2725cf8cb50cbbbfe
igarashi <igarashi@kuis.kyoto-u.ac.jp>
2016-11-13 18:46:41 +0900

    Add template files and submission instructions.

---
+++ java/Tree.java
@@ -0,0 +1,6 @@
+interface Tree {
+    public int value();
+    // public String toString();             // Kadai 1-3
+    // public String toStringFewerParens();  // Kadai 1-4
+    // public Tree reduce();                 // Kadai 1-5
+}
---
+++ ocaml/tree.ml
@@ -0,0 +1,16 @@
+type tree = (* FILL IN HERE *)
+
+(* Kadai 1-2 *)
+let value_of_tree(t) = (* FILL IN HERE *)
+
+(* Kadai 1-6
+let string_of_tree(t) = (* FILL IN HERE *)
+ *)
+
+(* Kadai 1-7
+let string_of_tree_fewer_parens(t) = (* FILL IN HERE *)
+ *)
+
+(* Kadai 1-8
+let reduce(t) = (* FILL IN HERE *)
+ *)
