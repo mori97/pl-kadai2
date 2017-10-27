1df341bfe80681d01358d20b1bae3c7d18a667c1
hatsusato <knightspm5@gmail.com>
2017-10-27 10:19:33 +0900

    Renumber exercise

--- ocaml/tree.ml
+++ ocaml/tree.ml
@@ -2,30 +2,29 @@ type tree = Leaf of int
           | BranchS of tree * tree
           | BranchM of tree * tree
 
-(* Kadai 1-8 *)
+(* Kadai 1-2-1 *)
 let value_of_tree(t) = (* FILL IN HERE *)
 
-(* Kadai 1-9 *)
+(* Kadai 1-2-2 *)
 let value_of_tree2(t) = (* FILL IN HERE *)
 
-(* Kadai 1-10 *)
+(* Kadai 1-2-3 *)
 let string_of_tree(t) = (* FILL IN HERE *)
 
-(* Kadai 1-11
+(* Kadai 1-2-4
 let string_of_tree_fewer_parens(t) = (* FILL IN HERE *)
  *)
 
-(* Kadai 1-12
+(* Kadai 1-2-5
 let count_leaf t = (* FILL IN HERE *)
 
 let height t = (* FILL IN HERE *)
  *)
 
-(* Kadai 1-13
+(* Kadai 1-2-6
 let reduce(t) = (* FILL IN HERE *)
  *)
 
-(* Kadai 1-14
+(* Kadai 1-2-7
 let subst(t, n, t') = (* FILL IN HERE *)
 *)
-
