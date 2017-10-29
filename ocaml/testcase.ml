63351259fa028064763cc2d5f1d717fa281ca5b9
hatsusato <knightspm5@gmail.com>
2017-10-29 18:21:23 +0900

    Add auxiliary functions

--- ocaml/testcase.ml
+++ ocaml/testcase.ml
@@ -12,3 +12,14 @@ let test_case () =
     BranchS (BranchM (Leaf 1, Leaf 2), BranchM (Leaf 3, Leaf 4));
     BranchM (BranchS (Leaf 1, BranchM (Leaf 2, Leaf 3)), BranchM (BranchM (Leaf 4, Leaf 5), BranchS (Leaf 6, Leaf 7)))
   ]
+
+let rec print_list f l i =
+  match l with
+    [] -> ()
+  | hd::tl ->
+     print_int i;
+     print_string ": ";
+     print_endline (f hd);
+     print_list f tl (i+1)
+let print_test_case f =
+  print_list f (test_case ()) 0
