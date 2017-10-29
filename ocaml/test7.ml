a841a04a54f95b9c1a74f6ce42d99354cd4e4ccc
hatsusato <knightspm5@gmail.com>
2017-10-29 18:27:48 +0900

    Add test7

---
+++ ocaml/test7.ml
@@ -0,0 +1,12 @@
+open Tree
+open Testcase
+
+let () =
+  let rec separate l =
+    match l with
+      [] -> []
+    | x::[] -> []
+    | x::y::tl -> (x, 3, y)::(separate tl) in
+  let l = separate (test_case ()) in
+  print_endline "[subst]";
+  print_list (fun t -> (string_of_tree (subst t))) l 0
