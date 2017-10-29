e9c49eba7a6c284a78484e1fcd9eff49110ce319
hatsusato <knightspm5@gmail.com>
2017-10-29 15:49:02 +0900

    Add Test6

---
+++ java/Test6.java
@@ -0,0 +1,10 @@
+public class Test6 {
+    public static void main(String[] args) {
+        Tree[] t = new TestCase().t;
+        System.out.println("[reduce]");
+        for (int i = 0; i < t.length; i++) {
+            System.out.print(i + ": ");
+            System.out.println(t[i].reduce());
+        }
+    }
+}
