5e145a18ad1594e4bb3cb8073d4a710acdc4f9fb
mori <mori@co-conv.jp>
2017-11-05 16:00:50 +0900

    [Ocaml] 課題 1-2-2
    tree にタイプ x のブランチを追加し、拡張された計算木を価値を計算する関数
    value_of_tree2 t を作成した。

--- ocaml/tree.ml
+++ ocaml/tree.ml
@@ -1,6 +1,7 @@
 type tree = Leaf of int
           | BranchS of tree * tree
           | BranchM of tree * tree
+          | BranchX of tree * tree
 
 (* Kadai 1-2-1 *)
 (* 計算木の価値を返す関数 *)
@@ -10,9 +11,16 @@ let rec value_of_tree t =
   | BranchS (left, right) -> value_of_tree left + value_of_tree right
   | BranchM (left, right) -> value_of_tree left * value_of_tree right;;
 
-(* Kadai 1-2-2
-let value_of_tree2(t) = (* FILL IN HERE *)
-*)
+(* Kadai 1-2-2 *)
+(* 計算木の価値を返す関数 *)
+let rec value_of_tree2 t =
+  let rec pow n m = (* n の m 乗 *)
+    if m <= 0 then 1 else n * pow n (m - 1) in
+  match t with
+    Leaf value -> value
+  | BranchS (left, right) -> value_of_tree2 left + value_of_tree2 right
+  | BranchM (left, right) -> value_of_tree2 left * value_of_tree2 right
+  | BranchX (left, right) -> pow (value_of_tree2 left) (value_of_tree2 right);;
 
 (* Kadai 1-2-3
 let string_of_tree(t) = (* FILL IN HERE *)
