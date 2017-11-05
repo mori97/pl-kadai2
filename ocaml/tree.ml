651769589328d81932aa833b8a287d3e747b6b7a
mori <mori@co-conv.jp>
2017-11-05 15:37:07 +0900

    [OCaml]
    comment all functions

--- ocaml/tree.ml
+++ ocaml/tree.ml
@@ -2,14 +2,17 @@ type tree = Leaf of int
           | BranchS of tree * tree
           | BranchM of tree * tree
 
-(* Kadai 1-2-1 *)
+(* Kadai 1-2-1
 let value_of_tree(t) = (* FILL IN HERE *)
+*)
 
-(* Kadai 1-2-2 *)
+(* Kadai 1-2-2
 let value_of_tree2(t) = (* FILL IN HERE *)
+*)
 
-(* Kadai 1-2-3 *)
+(* Kadai 1-2-3
 let string_of_tree(t) = (* FILL IN HERE *)
+*)
 
 (* Kadai 1-2-4
 let string_of_tree_fewer_parens(t) = (* FILL IN HERE *)
