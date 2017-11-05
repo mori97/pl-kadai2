a25b54c76c8b51a6ec35a86b321182d6cbb8de41
mori <mori@co-conv.jp>
2017-11-05 17:17:53 +0900

    [OCaml] テストケースの追加
    Java 側で追加したテストケースをそのまま持ってきた。

--- ocaml/testcase.ml
+++ ocaml/testcase.ml
@@ -11,6 +11,13 @@ let test_case =
     BranchM (Leaf 6, Leaf 7);
     BranchS (BranchM (Leaf 1, Leaf 2), BranchM (Leaf 3, Leaf 4));
     BranchM (BranchS (Leaf 1, BranchM (Leaf 2, Leaf 3)), BranchM (BranchM (Leaf 4, Leaf 5), BranchS (Leaf 6, Leaf 7)));
+    (* 課題 1-1-3 *)
+    BranchS (Leaf 2, BranchX (Leaf 2, BranchM (Leaf 2, Leaf 3)));
+    (* 課題 1-1-4 *)
+    BranchX (Leaf 3, BranchX (Leaf 3, Leaf 3));
+    BranchX (BranchX (Leaf 3, Leaf 3), Leaf 3);
+    (* 課題 1-1-7 用(t が偶数個の要素を持たないといけないみたい) *)
+    BranchS (Leaf 1, Leaf 2)
   ]
 
 let rec print_list f l i =
